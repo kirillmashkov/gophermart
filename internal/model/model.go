e1d47debc163d8f69f8b7206ae70ba85c90e50a2
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-06-30 13:56:52 +0300

    fix select null values

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,6 +1,8 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type LoginPassRequest struct {
 	Login    string `json:"login"`
@@ -32,20 +34,20 @@ type AccrualResponse struct {
 type OrdersDB struct {
 	Number     string
 	Status     string
-	Sum        float32
+	Sum        *float32
 	UploadedAt time.Time
 }
 
 type OrdersBalanceReposponse struct {
 	Number     string    `json:"number"`
 	Status     string    `json:"status"`
-	Accrual    float32       `json:"accrual"`
+	Accrual    *float32  `json:"accrual,omitempty"`
 	UploadedAt time.Time `json:"uploaded_at"`
 }
 
 type OrderWithdrawlResponse struct {
 	Order       string    `json:"order"`
-	Sum         float32       `json:"sum"`
+	Sum         *float32  `json:"sum"`
 	ProcessedAt time.Time `json:"processed_at"`
 }
 
