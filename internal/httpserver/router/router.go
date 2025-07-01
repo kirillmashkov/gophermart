fdd9fa5b175516ffa08d58041987366163413bc3
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-07-01 16:20:08 +0300

    return own compress middleware

--- internal/httpserver/router/router.go
+++ internal/httpserver/router/router.go
@@ -5,17 +5,15 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/handler"
-	// "github.com/kirillmashkov/gophermart/internal/httpserver/middleware/compress"
+	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/compress"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/logger"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/security"
-	"github.com/go-chi/chi/middleware"
 )
 
 func Serv() http.Handler {
 	r := chi.NewRouter()
 	r.Use(logger.Logger)
-	// r.Use(compress.Compress)
-	r.Use(middleware.Compress(5, "gzip"))
+	r.Use(compress.Compress)
 	r.Use(security.Auth)
 
 	r.Post("/api/user/register", handler.RegisterUser)
@@ -25,9 +23,5 @@ func Serv() http.Handler {
 	r.Get("/api/user/withdrawals", handler.GetWithdrawalsOrders)
 	r.Get("/api/user/balance", handler.GetBalance)
 	r.Post("/api/user/balance/withdraw", handler.CreateWithdrawnOrder)
-	// r.Post("/api/shorten/batch", handler.PostGenerateShortURLBatch)
-	// r.Delete("/api/user/urls", handler.DeleteURLBatch)
-	// r.Get("/ping", handler.Ping)
-
 	return r
 }
