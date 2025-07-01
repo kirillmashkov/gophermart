ceba23e02160fec2ccc6433a59f51af9797f0d9b
Kirill Mashkov <kirill.mashkov@x5.ru>
2025-07-01 15:38:04 +0300

    use chi compress

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.24.2
 
 require (
 	github.com/caarlos0/env/v6 v6.10.1 // indirect
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/golang-migrate/migrate/v4 v4.18.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
--- internal/httpserver/router/router.go
+++ internal/httpserver/router/router.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/handler"
-	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/compress"
+	// "github.com/kirillmashkov/gophermart/internal/httpserver/middleware/compress"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/logger"
 	"github.com/kirillmashkov/gophermart/internal/httpserver/middleware/security"
+	"github.com/go-chi/chi/middleware"
 )
 
 func Serv() http.Handler {
 	r := chi.NewRouter()
 	r.Use(logger.Logger)
-	r.Use(compress.Compress)
+	// r.Use(compress.Compress)
+	r.Use(middleware.Compress(5, "gzip"))
 	r.Use(security.Auth)
+
 	r.Post("/api/user/register", handler.RegisterUser)
 	r.Post("/api/user/login", handler.LoginUser)
 	r.Post("/api/user/orders", handler.CreateOrder)
